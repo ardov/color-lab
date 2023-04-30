8d0b0cf16ab14cd529d5d406d5da0ba5e3d078c3
Alexey Ardov <ardovalexey@gmail.com>
2023-04-30 21:11:30 +0200

    set up config for XR

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [basicSsl(), tsconfigPaths(), react()],
+  server: {
+    https: true,
+  },
 })
