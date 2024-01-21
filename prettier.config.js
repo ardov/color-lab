1819375fe3a6048f7fc438074e66b3162d382dca
Alexey Ardov <ardovalexey@gmail.com>
2024-01-21 01:08:53 +0100

    try playing with tailwind

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tailwindFunctions: ['clsx', 'cva'],
+  plugins: ['eslint-plugin-prettier', 'prettier-plugin-tailwindcss'],
+}
